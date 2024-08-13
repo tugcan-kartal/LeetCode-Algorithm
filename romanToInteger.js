978193d9c53d087ca1c8cf69941c3eaa01e8cbd0
tugcan-kartal <tugcankartal01@gmail.com>
2024-08-13 16:17:04 +0300

    added romanToInteger algorithm solution

---
+++ romanToInteger.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var romanToInt=(s)=>{
+    romanMap={
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+    }
+
+    let total=0;
+    let currentValue=0;
+    let prevValue=0;
+
+    for(let i=s.length-1;i>=0;i--){
+        
+        currentValue=romanMap[s[i]];
+        
+        if(currentValue>=prevValue){
+            total+=currentValue;
+        }else{
+            total-=currentValue;
+        }
+
+        prevValue=currentValue;
+    }
+
+    return total;
+}
