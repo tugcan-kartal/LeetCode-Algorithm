01b5a491ea480b7ab28282d50a951751a6723f40
tugcan-kartal <tugcankartal01@gmail.com>
2024-08-28 14:53:23 +0300

    added sqrt algorithm solution

---
+++ Sqrt(x).js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if(x<2){
+        return x;
+    }
+
+    let left=1;
+    let right=x;
+    let result=0;
+
+    while(left<=right){
+        let mid=Math.floor((left+right)/2);
+        if(mid*mid===x){
+            return mid;
+        }else if(mid*mid<x){
+            left=mid+1;
+            result=mid;
+        }else{
+            right=mid-1;
+        }
+    }
+    return result;;
+};
